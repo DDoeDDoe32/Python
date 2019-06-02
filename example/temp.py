da0e48043aa5a2de69ce4d4c5c1f95d18f1a0043
DDoeDDoe32 <42180081+DDoeDDoe32@users.noreply.github.com>
2019-06-02 22:21:47 +0900

    파이썬_변수

--- example/temp.py
+++ example/temp.py
@@ -1,3 +1,17 @@
-print('Hello python')
-print("Hello World")
-print(1234567890)
+myStr = 'Hello python'
+myChar = 'a'
+myInt = 123
+myFloat = 3.14
+myBool = True
+
+print("myStr = ", myStr)
+#print("myChar = ", myChar)
+print("myInt = ", myInt)
+'''
+print("myFloat = ", myFloat)
+print("myBool = ", myBool)
+'''
+"""
+print("myFloat = ", myFloat)
+print("myBool = ", myBool)
+"""

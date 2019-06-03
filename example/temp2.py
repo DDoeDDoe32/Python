3c751545a9c706bcfeb800b30038b6c2ddf5cd58
DDoeDDoe32 <42180081+DDoeDDoe32@users.noreply.github.com>
2019-06-03 22:02:22 +0900

    파이썬_데이터타입

---
+++ example/temp2.py
@@ -0,0 +1,34 @@
+"""
+myInt = 12345678901234567890123456789
+print(myInt)
+
+print(type(myInt))
+
+myStr = 'Hello python'
+print(myStr)
+print(type(myStr))
+
+myInt = 1568 + 10
+print(myInt)
+"""
+"""
+myFloat = 1.23456789012345678901234567890123
+
+print(type(myFloat))
+print(myFloat)
+"""
+'''
+myStr = 'my String'
+print(myStr)
+print(type(myStr))
+
+
+myBool = True
+print(myBool)
+print(type(myBool))
+'''
+
+myStr = '123' #문자열
+myInt = 456 #정수
+print(int(myStr) + myInt)
+print(myStr + str(myInt))

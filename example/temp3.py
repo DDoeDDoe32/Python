ed0a2367d997c9873628616e5e7a735b9d5d39de
DDoeDDoe32 <42180081+DDoeDDoe32@users.noreply.github.com>
2019-06-04 23:48:09 +0900

    파이썬_컨테이너자료형

---
+++ example/temp3.py
@@ -0,0 +1,58 @@
+myList = ['abc', 123, 3.14, "a", True]
+"""
+print(myList)
+
+print(myList[0])
+
+print(len(myList))
+
+myList.append('Hello World')
+myList.append('Hello World')
+
+print(myList)
+
+myList.pop()
+myList.pop(2)
+
+print(myList)
+"""
+"""
+print(myList)
+
+myList.extend(['a', 'b', 'c'])
+
+print(myList)
+
+myList.insert(1, 'new data')
+
+print(myList)
+
+myList.remove('new data')
+
+print(myList)
+
+myList.clear()
+
+print(myList)
+"""
+sortList = ['abc', 'grt', 'hgw', 'asdfa']
+
+sortList.sort(reverse = False)
+
+print(sortList)
+
+sortList.sort(reverse = True)
+
+print(sortList)
+
+sortList.reverse()
+
+print(sortList)
+
+print(sortList[1:3])
+
+print(sortList[0:4])
+
+print(sortList[1:2])
+
+

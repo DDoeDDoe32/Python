f5c23efcff1b1533cb7c6ceb345590a8766ebfde
DDoeDDoe32 <42180081+DDoeDDoe32@users.noreply.github.com>
2019-06-05 20:24:31 +0900

    파이썬_연산자

---
+++ example/temp4.py
@@ -0,0 +1,31 @@
+"""
+num1 = 20
+num2 = 30
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+
+print('a'+'b')
+"""
+'''
+print(10 == 200)
+print(10 > 200)
+print(10 >= 200)
+print(10 < 200)
+print(10 <= 200)
+print(10 != 200)
+
+print('c' < 'p') #아스키코드
+'''
+
+print(not True)
+
+print(True and True)
+
+print(True and False)
+
+print(False and False)
+
+print(True or False)

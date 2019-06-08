d502acca384801136f8650ba70e8d9e658b41c6e
DDoeDDoe32 <42180081+DDoeDDoe32@users.noreply.github.com>
2019-06-08 21:58:17 +0900

    파이썬_if문

---
+++ example/temp5.py
@@ -0,0 +1,28 @@
+'''
+num1 = 20
+num2 = 30
+
+if(num1 > num2) : #False
+    print('{0} > {1}'.format(num1, num2))
+
+else :
+    print('{0} <= {1}'.format(num1, num2))
+'''
+'''
+if(num1 == num2) : #False
+    print('{0} == {1}'.format(nu1, num2))
+'''
+    
+score = 70
+
+if(score >= 90) :
+    print('수')
+elif(score >= 80) :
+    print('우')
+elif(score >= 70) :
+    print('미')
+elif(score >= 60) :
+    print('양')
+else :
+    print('가')
+    

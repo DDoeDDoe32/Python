6343759060d56cecfd36463d46c64d4e59dac11c
DDoeDDoe32 <42180081+DDoeDDoe32@users.noreply.github.com>
2019-07-12 00:14:27 +0900

    파이썬_객체지향3

--- example/temp10.py
+++ example/temp10.py
@@ -1,1 +1,12 @@
-#
+class ParentCls :
+    def __init__(self) :
+        pass
+    def printMethod(self) :
+        print('Hello python')
+
+class childClass(ParentCls) :
+    def __init__(self) :
+        pass
+
+childCls = childClass()
+childCls.printMethod()
